fdcc0984cfcf8572ba8cee087ca7598314ac9aee
Jonathan Buchholz <63243849+JonathanBuchh@users.noreply.github.com>
2021-07-18 00:14:32 -0400

    Make current page green in navigation

--- scripts/nav.js
+++ scripts/nav.js
@@ -3,3 +3,11 @@ var navDropdownButton = document.getElementsByClassName('nav-dropdown-button')[0
 navDropdownButton.addEventListener('click', function() {
     nav.classList.toggle('open');
 });
+
+// Go through all the nav items; if the nav item is currently active give it the .active class to highlight it
+var navItems = document.querySelectorAll('nav a');
+for (i = 0; i < navItems.length; i++) {
+    if (navItems[i].href.endsWith(window.location.pathname)) {
+        navItems[i].className = 'active';
+    }
+}
