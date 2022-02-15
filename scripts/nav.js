80124b1739d78289589f9af4f758a354cd8e12f1
Jonathan Buchholz <jonathan@buchh.org>
2022-02-14 23:07:12 -0500

    Add Refugee Resettlement to nav bar

--- scripts/nav.js
+++ scripts/nav.js
@@ -10,5 +10,10 @@ for (i = 0; i < navItems.length; i++) {
     var match = navItems[i].href.match(new RegExp('\\w+\\/([\\/\\w-]+)'));
     if (match && window.location.pathname.includes(match[1])) {
         navItems[i].className = 'active';
+        // The Refugee Resettlement page is listed twice, so the highest level should be active. I have no knowledge of JavaScript, so there is probably a better way to do this--this is just what my limited knowledge and internet searching got me:
+        if (window.location.pathname.includes("refugee-resettlement")) {
+            var unactive = document.getElementById("community-service");
+            unactive.classList.remove("active");
+        }
     }
 }
